8058f8e9f12a08c89f491a4f6b29ce2afaebbafa
WasiqueS <zeyaanshaikh86@gmail.com>
2025-06-23 18:20:42 +0530

    manifest file added

---
+++ addons/thames_config/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': "Thames ERP Configuration",
+    'summary': "Installs all the required modules for the Thames ERP project.",
+    'description': """
+        This module acts as the master configuration for any new Thames ERP deployment.
+        Installing this single module will automatically install all other required apps.
+    """,
+    'author': "Your Company Name",
+    'website': "https://www.thameserp.com",
+    'category': 'Uncategorized',
+    'version': '1.0',
+
+    # List the technical names of the apps you installed.
+    'depends': [
+        'base',
+        'crm',
+        'sale_management',
+        'stock',
+        'account',
+    ],
+
+    'installable': True,
+    'application': True,
+}
