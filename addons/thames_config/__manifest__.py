117bb45e4f859e5686ed46b7b871e08cc973f3fc
WasiqueS <zeyaanshaikh86@gmail.com>
2025-06-24 20:27:39 +0530

    some changes done

--- addons/thames_config/__manifest__.py
+++ addons/thames_config/__manifest__.py
@@ -1,20 +1,15 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "Thames ERP Configuration",
-    'summary': "Installs all required modules for the Thames ERP project.",
-    'author': "Your Company Name",
-    'website': "https://www.thameserp.com",
-    'category': 'Uncategorized',
+    'name': 'Thames CRM Customizations',
     'version': '1.0',
-    # List of all required applications plus our customizations
-    'depends': [
-        'crm',
-        'sale_management',
-        'website',
-        'stock',
-        'account', # This is the technical name for the Invoicing app
-        'thames_crm_customizations', # Your customizations module
+    'summary': 'Adds custom fields to the customer form.',
+    'category': 'Sales/CRM',
+    'author': 'Thames CC',
+    'website': 'https://www.thamescc.com',
+    'depends': ['base', 'contacts'],
+    'data': [
+        'views/res_partner_views.xml',
     ],
     'installable': True,
-    'application': True,
+    'application': False,
+    'auto_install': False,
 }
