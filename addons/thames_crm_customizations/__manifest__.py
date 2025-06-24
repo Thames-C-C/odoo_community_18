27a721444fbcd87953da9f07975e4d3c9adf8db4
WasiqueS <zeyaanshaikh86@gmail.com>
2025-06-24 22:46:51 +0530

    some changes done

---
+++ addons/thames_crm_customizations/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Thames CRM Customizations',
+    'version': '1.0',
+    'summary': 'Adds custom fields to the customer form.',
+    'category': 'Sales/CRM',
+    'author': 'Thames CC',
+    'website': 'https://www.thamescc.com',
+    'depends': ['base', 'contacts'],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+} 
